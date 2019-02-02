091be5bdeef564d6720895db78d607cb250e7554
Aishiming <shiming@ifastfinancial.com.cn>
2019-02-02 16:07:56 +0800

    add basic hystrix

--- spring-cloud-eureka/spring-cloud-consumer/src/main/java/com/asm/service/HelloRemote.java
+++ spring-cloud-eureka/spring-cloud-consumer/src/main/java/com/asm/service/HelloRemote.java
@@ -1,13 +1,14 @@
 package com.asm.service;
 
+import com.asm.service.hyxtrix.HelloRemoteHystrix;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "spring-cloud-producer")
+@FeignClient(name = "spring-cloud-producer", fallback = HelloRemoteHystrix.class)
 public interface HelloRemote {
 
     @RequestMapping(value = "/hello")
-    public String sayHello(@RequestParam(value = "name") String name);
+    public String sayHello(@RequestParam(value = "name") final String name);
 
 }
